19925d68b3801c78b4bdee66c7a8b3099f9736e3
Thomas22p <thomas.poupon@edu.univ-fcomte.fr>
2024-01-27 18:51:23 +0100

    curseur et essai de chargement

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,80 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
-import './assets/style.css'
+  import { ref, onMounted } from 'vue'
+  import { RouterLink, RouterView } from 'vue-router'
+  import './assets/style.css'
+  // import LoadingPage from './components/LoadingPage.vue'
 
-const customCursor = ref(null)
+  const customCursor = ref(null)
 
-onMounted(() => {
-  animateCursor()
-})
+  // const isFirstVisit = ref(true)
 
-const animateCursor = () => {
+  onMounted(() => {
+    animateCursor()
+
+    // Vérifiez si c'est la première visite en utilisant le localStorage
+  // const visitedBefore = localStorage.getItem('visitedBefore')
+
+  // if (visitedBefore) {
+  //   isFirstVisit.value = false
+  // } else {
+  //   // Si c'est la première visite, enregistrez cette information dans le localStorage
+  //   localStorage.setItem('visitedBefore', true)
+  // }
+
+  })
+
+
+
+  // curseur personnalisé 
+
+  const animateCursor = () => {
   window.addEventListener("mousemove", handleMouseMove)
-}
-
-const handleMouseMove = (e) => {
-  if (customCursor.value) {
-    const posX = e.clientX;
-    const posY = e.clientY;
-
-    const offsetX = -10;
-    const offsetY = -10;
-
-    customCursor.value.style.transform = `translate(${posX + offsetX}px, ${posY + offsetY}px)`;
-
-    // Utilise requestAnimationFrame pour une animation plus fluide
-    requestAnimationFrame(animateCursor)
   }
-}
+
+  const handleMouseMove = (e) => {
+    if (customCursor.value) {
+      const posX = e.clientX;
+      const posY = e.clientY;
+
+      const offsetX = -16;
+      const offsetY = -16;
+
+      customCursor.value.style.transform = `translate(${posX + offsetX}px, ${posY + offsetY}px)`;
+    }
+  }
+
+
+
+  // ...
+
 </script>
 
 <template>
-  <header>
+  <div>
+    <header>
+      <!-- <LoadingPage v-if="isFirstVisit" /> -->
+      <div>
+        <div class="custom-cursor" ref="customCursor"></div>
+        <nav>
+          <RouterLink to="/" class="text-slate-100">Home</RouterLink>
+          <RouterLink to="/about" class="text-slate-100">About</RouterLink>
+          <RouterLink to="/contact" class="text-slate-100">Contact</RouterLink>
+        </nav>
 
-    <div>
+        <RouterView />
+      </div>
+    </header>
 
-      <div class="circle" ref="customCursor"></div>
-
-      <nav>
-        <RouterLink to="/" class="text-slate-100">Home</RouterLink>
-        <RouterLink to="/about" class="text-slate-100">About</RouterLink>
-        <RouterLink to="/contact" class="text-slate-100">Contact</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
+
+
+<style>
+.light-section {
+  background-color: #f9f9f9;
+  padding: 20px;
+  margin: 20px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+}
+</style>
